d21129a4055a070bf98c2d894cdd65b729d0b69b
Alon Yakobichvilli <alon.yakoby@gmail.com>
2024-05-28 09:42:48 +0200

    updated configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,7 +2,7 @@ import Fluent
 import FluentMongoDriver
 import Leaf
 import Vapor
-//import JWT
+import CORSSupport
 
 extension String {
     var bytes: [UInt8] { .init(self.utf8) }
@@ -12,8 +12,7 @@ extension String {
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    
-    //    app.jwt.signers.use(.hs256(key: "3Cz30pJzxbqYvLjXqTJjU8VpU5bxvgoNRvq1a+BXOts"))
+
     let encoder = JSONEncoder()
     encoder.keyEncodingStrategy = .convertToSnakeCase
     encoder.dateEncodingStrategy = .iso8601
@@ -24,10 +23,8 @@ public func configure(_ app: Application) throws {
 
     ContentConfiguration.global.use(encoder: encoder, for: .json)
     ContentConfiguration.global.use(decoder: decoder, for: .json)
-    
-//    app.jwt.signers.use(.hs256(key: Environment.get(ENV.jwtSecret.key) ?? ENV.jwtSecret.dev_default))
 
-    try app.databases.use(.mongo(connectionString:Environment.get(ENV.databaseURL.key) ?? ENV.databaseURL.dev_default),
+    try app.databases.use(.mongo(connectionString: Environment.get(ENV.databaseURL.key) ?? ENV.databaseURL.dev_default),
                           as: .mongo)
  
     app_migrations.forEach { app.migrations.add($0) }
@@ -35,19 +32,16 @@ public func configure(_ app: Application) throws {
     try app.autoMigrate().wait()
  
     app.views.use(.leaf)
-//    app.middleware.use(DBUser.authenticator())
-//    app.middleware.use(Token.authenticator())
-
-    
     app.middleware.use(ErrorMiddleware.default(environment: app.environment))
     app.passwords.use(.bcrypt)
 
+    // CORS configuration
     let corsConfiguration = CORSMiddleware.Configuration(
-        allowedOrigin: .all, // Allow all origins
-        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH], // Specify allowed methods
-        allowedHeaders: [.authorization, .contentType, .accept, .origin, .xRequestedWith], // Specify allowed headers
-        allowCredentials: true, // Whether to allow cookies/cross-origin requests
-        exposedHeaders: [.authorization, .contentType] // Optional: Specify headers that browsers are allowed to access
+        allowedOrigin: .custom("http://165.232.91.105:3000"), // Specify the exact origin
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.authorization, .contentType, .accept, .origin, .xRequestedWith],
+        allowCredentials: true,
+        exposedHeaders: [.authorization, .contentType]
     )
 
     let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
